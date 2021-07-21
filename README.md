8e92246f3538104f30e91831c470f1146f3accda
stscoundrel <sampo@pixels.fi>
2021-07-21 17:40:51 +0300

    Add src for 2038 bug demo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# year-2038-problem
-Demonstration of year 2038 problem in Rust
+# Year 2038 problem
+
+Demonstration of year 2038 problem in Rust.
+
+The year 2038 problem refers to maximum unix date that can be stored in i32 value. 03:14:07 UTC on 19 January 2038, the integer will overflow. Result will be incorrect time calculation, around 20:45:52 on Friday, 13 December 1901.
+
+## The test
+
+- Creates maximum value for i32
+- Uses wrapped add to increase it by 1 without causing overflow errors
+- In very simplified way, parse current year from the timestamp. Ignores constraints like leap years, leap seconds etc.
+
+## To run
+
+`cargo run`
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+// Very naive year calculation, ignores leap years etc.
+fn get_current_year(timestamp: i32) -> i32 {
+    const START_YEAR: i32 = 1970;
+    let timestamp_in_years = timestamp / 60 / 60 / 24 / 365;
+
+    START_YEAR + timestamp_in_years
+}
+
+fn main() {
+    let max_timestamp: i32 = i32::MAX; // 19.11.2038 in unix
+    let past_max_timestamp = max_timestamp.wrapping_add(1); // Second past max
+
+    let max_date = get_current_year(max_timestamp);
+    let past_max_date = get_current_year(past_max_timestamp);
+
+    println!("----------------------------------------------------------------");
+    println!("Should print year from max timestamp. Real value should be 2038");
+    println!("Max timestamp: {}", max_date); 
+    println!("----------------------------------------------------------------");
+    println!("Should print year from max timestamp + 1 second. Logically it should still be 2038");
+    println!("Past max timestamp: {}", past_max_date);
+    println!("Due to integer overflow, we're in 1900s. With proper date calc it would be 1901, but less-than-precise check brings us to 1092.");
+    println!("Still far cry from 2038, which would be the 'logical' value");
+    println!("----------------------------------------------------------------");
+}
